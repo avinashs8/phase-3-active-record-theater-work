bd51e16e82673953fd0b0f22da89bad5a7274036
avinash singh <avinash8.as@gmail.com>
2023-05-05 20:53:51 -0400

    commit tree 142e30811cb30108520b1921afb47e65580bb815

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+puts "creating roles"
+Role.destroy_all
+
+5.times {Role.create(character_name: Faker::Name.name)}
+
+puts "creating auditions..."
+
+25.times {Audition.create(role_id:rand(1...5), actor: Faker::Name.name, location: Faker::Address.city, hired: false, phone: 9999999999)}
